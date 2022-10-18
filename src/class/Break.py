0cd153e640e084b573c336d6af1e592fb48ed85a
SvetlanaYes <yesayan.svetlana@studend.rau.am>
2022-10-18 22:02:50 +0400

    some changes

---
+++ src/class/Break.py
@@ -0,0 +1,6 @@
+class Break:
+    def __init__(self, start, end, duration):
+        self.start = start
+        self.end = end
+        self.duration = duration
+
---
+++ utils/CartesianProduct.py
@@ -0,0 +1,18 @@
+def cartesianHelper(a, b):
+    result = []
+    for i in range(0, len(a)):
+        for j in range(0, len(b)):
+            if type(a[i]) != list:
+                a[i] = [a[i]]
+            temp = [num for num in a[i]]
+            temp.append(b[j])
+            result.append(temp)
+    return result
+
+
+def Cartesian(arr, n):
+    all_possible_combinations = arr[0]
+    for i in range(1, n):
+        all_possible_combinations = cartesianHelper(all_possible_combinations, arr[i])
+    return all_possible_combinations
+
